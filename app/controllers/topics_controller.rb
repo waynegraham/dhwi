8a0cae08fdd16e85207e83ad1bcd7fc67e8c8a4c
Daniel Powell <djpowell@uvic.ca>
2013-01-10 14:24:07 -0500

    Styling & formatting changes

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -44,7 +44,7 @@ class TopicsController < ApplicationController
 
     respond_to do |format|
       if @topic.save
-        format.html { redirect_to topics_path, notice: 'Topic was successfully created.' }
+        format.html { redirect_to topics_path, notice: 'Beer was successfully listed.' }
         format.json { render json: @topic, status: :created, location: @topic }
       else
         format.html { render action: "new" }
@@ -60,7 +60,7 @@ class TopicsController < ApplicationController
 
     respond_to do |format|
       if @topic.update_attributes(params[:topic])
-        format.html { redirect_to @topic, notice: 'Topic was successfully updated.' }
+        format.html { redirect_to @topic, notice: 'Beer was successfully listed.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
